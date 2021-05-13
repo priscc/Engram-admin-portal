a121c7e3457aed373c8e99fd431194c08e509822
Priscilla Cancar <pncancar@gmail.com>
2021-05-12 22:57:39 -0400

    clearing fiels

--- src/utils/filter.js
+++
@@ -1,7 +0,0 @@
-export const filterByValue = (array, string) => {
-  return array.filter((o) =>
-    Object.keys(o).some((k) =>
-      o[k].toLowerCase().includes(string.toLowerCase())
-    )
-  );
-};
